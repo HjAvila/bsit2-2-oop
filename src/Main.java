995e5e48b375746b005a80dcbb3a07b68bf4b155
Hans Jerald Avila <hjavila86122@liceo.edu.ph>
2025-08-01 09:57:25 +0800

    add main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
+
 public class Main  {
     public static void main(String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+        int total = 0;
 
-        System.out.println("hello world");
+        System.out.println("INPUT A 5 NUMBER");
+        for (int i = 1; i <= 5; i++) {
 
+            System.out.print("Enter number " + i + ": ");
+            int number = scanner.nextInt();
+            total += number;
+        }
 
+        System.out.println();
+        System.out.println("Total Number: " + total);
+
+        scanner.close();
     }
 }
+
+
+
+
