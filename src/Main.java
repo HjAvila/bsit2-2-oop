fd29b140f43f2763dcd46442d6054bd3feddd988
HjAvila <hjavila86122@liceo.edu.ph>
2025-08-04 17:41:18 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
-public class Main  {
-    public static void main(String[] args ) {
+public class Main {
+    public static void main(String[] args) {
+        Book book1 = new Book("The Alchemist", "Paulo Coelho", 208);
+        Book book2 = new Book("1984", "George Orwell", 328);
+        Book book3 = new Book("To Kill a Mockingbird", "Harper Lee", 281);
 
-        System.out.println("hello world");
+        book1.displayInfo();
+        book2.displayInfo();
+        book3.displayInfo();
 
+        book1.borrowBook();
+        book1.displayInfo();
 
+        book1.returnBook();
+        book1.displayInfo();
     }
 }
