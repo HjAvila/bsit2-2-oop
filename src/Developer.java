62ee9c3fc3088983338bc4a48e79479b47454d45
HjAvila <hjavila86122@liceo.edu.ph>
2025-09-11 11:48:24 +0800

    Update Developer.java

--- src/Developer.java
+++ src/Developer.java
@@ -1,27 +1,24 @@
 public class Developer extends Employee {
-    private String programmingLanguage;
-    private int projectsCompleted;
+    String programmingLanguage;
+    int projectsCompleted;
 
-    public Developer(String name, int employeeId, double baseSalary, String department, String programmingLanguage, int projectsCompleted) {
-        super(name, employeeId, baseSalary, department);
+    Developer(String name, int id, double baseSalary, String department, String programmingLanguage, int projectsCompleted) {
+        super(name, id, baseSalary, department);
         this.programmingLanguage = programmingLanguage;
         this.projectsCompleted = projectsCompleted;
         System.out.println(name + " joined as a " + programmingLanguage + " Developer");
     }
 
-    @Override
-    public double calculateSalary() {
-        return baseSalary + (projectsCompleted * 1000);
-    }
-
-    @Override
-    public void work() {
+    void code() {
         System.out.println(name + " is coding in " + programmingLanguage);
     }
 
-    @Override
-    public void displayInfo() {
-        super.displayInfo();
+    double calculateSalary() {
+        return baseSalary + (projectsCompleted * 1000);
+    }
+
+    void displayDetails() {
+        super.displayDetails();
         System.out.println("Programming Language: " + programmingLanguage);
         System.out.println("Projects Completed: " + projectsCompleted);
     }
