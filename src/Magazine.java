5d537b88cb90713b8304e65218b613d27cf56f16
HjAvila <hjavila86122@liceo.edu.ph>
2025-10-03 09:48:31 +0800

    Update Magazine.java

--- src/Magazine.java
+++ src/Magazine.java
@@ -1,15 +1,55 @@
-public class Magazine extends Media {
+public class Magazine extends LibraryItem implements Borrowable {
     private int issueNumber;
+    private String publicationMonth;
+    private boolean isLatestIssue;
 
-    public Magazine(String title, String itemId, int issueNumber) {
-        super(title, itemId);
-        this.issueNumber = issueNumber;
+    public Magazine(String itemId, String title, String author, int issueNumber, String publicationMonth, boolean isLatestIssue) {
+        super(itemId, title, author);
+        setIssueNumber(issueNumber);
+        this.publicationMonth = publicationMonth;
+        this.isLatestIssue = isLatestIssue;
     }
 
-    public String getMediaType() {
-        return "Magazine";
+    public int getIssueNumber() { return issueNumber; }
+    public String getPublicationMonth() { return publicationMonth; }
+    public boolean isLatestIssue() { return isLatestIssue; }
+
+    public void setLatestIssue(boolean latest) { this.isLatestIssue = latest; }
+
+    public void setIssueNumber(int issue) {
+        if (issue <= 0) throw new IllegalArgumentException("Issue number must be positive");
+        this.issueNumber = issue;
     }
 
+    @Override
+    public String getItemType() { return "Magazine"; }
+
+    @Override
+    public double calculateLateFee(int daysLate) {
+        return daysLate * 0.25;
+    }
+
+    @Override
+    public void borrowItem(String borrowerName) {
+        checkOut(borrowerName);
+    }
+
+    @Override
+    public void returnItem() {
+        checkIn();
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return !isCheckedOut;
+    }
+
+    @Override
+    public int getBorrowingPeriod() {
+        return 7;
+    }
+
+    @Override
     public void displayInfo() {
         System.out.println("Magazine: \"" + title + "\" Issue #" + issueNumber + " (ID: " + itemId + ")");
     }
