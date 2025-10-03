ab6e7c9c755d18ce69a8e1ad3c60a4a6df68cf3f
HjAvila <hjavila86122@liceo.edu.ph>
2025-10-03 09:59:43 +0800

    Update Faculty.java

--- src/Faculty.java
+++ src/Faculty.java
@@ -15,23 +15,21 @@ public class Faculty extends User {
     public int getMaxBorrowLimit() { return 10; }
 
     public void borrowMedia(LibraryItem media) {
-        if (media instanceof Borrowable) {
-            Borrowable b = (Borrowable) media;
-            if (b.isAvailable() && getBorrowedItemsCount() < getMaxBorrowLimit()) {
-                b.borrowItem(getName());
-                addBorrowedItem(media);
-                System.out.println(getName() + " (Faculty) borrowed: " + media.getTitle());
-            } else {
-                System.out.println("Sorry, " + media.getTitle() + " is not available");
-            }
+        if (!(media instanceof Borrowable)) return;
+        Borrowable b = (Borrowable) media;
+        if (b.isAvailable() && getBorrowedItemsCount() < getMaxBorrowLimit()) {
+            b.borrowItem(getName());
+            addBorrowedItem(media);
+            System.out.println(getName() + " (Faculty) borrowed: " + media.getTitle());
+        } else {
+            System.out.println("Sorry, " + media.getTitle() + " is not available");
         }
     }
 
     public void returnMedia(LibraryItem media) {
-        if (media instanceof Borrowable) {
-            Borrowable b = (Borrowable) media;
-            b.returnItem();
-            removeBorrowedItem(media);
-        }
+        if (!(media instanceof Borrowable)) return;
+        Borrowable b = (Borrowable) media;
+        b.returnItem();
+        removeBorrowedItem(media);
     }
 }
