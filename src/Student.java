b0c7a49159dca6776ff50ad6f616b2d810bb43eb
HjAvila <hjavila86122@liceo.edu.ph>
2025-10-03 09:41:41 +0800

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,30 +1,53 @@
-public class Student implements LibraryUser {
-    private String name;
+public class Student extends User implements Borrowable {
     private String studentId;
+    private String major;
 
-    public Student(String name, String studentId) {
-        this.name = name;
+    public Student(String userId, String name, String email, String studentId, String major) {
+        super(userId, name, email);
         this.studentId = studentId;
+        this.major = major;
     }
 
-    public String getUserType() {
-        return "Student";
+    public String getStudentId() { return studentId; }
+    public String getMajor() { return major; }
+
+    @Override
+    public int getMaxBorrowLimit() { return 5; }
+
+    @Override
+    public void borrowItem(String borrowerName) {
+        
     }
 
-    public void borrowMedia(Media media) {
-        if (media.isAvailable()) {
-            media.borrowItem();
-            System.out.println(name + " (" + getUserType() + ") borrowed: " + media.getTitle());
-        } else {
-            System.out.println("Sorry, " + media.getTitle() + " is not available");
+    @Override
+    public void returnItem() {
+        
+    }
+
+    @Override
+    public boolean isAvailable() { return false; }
+
+    @Override
+    public int getBorrowingPeriod() { return 0; }
+
+    public void borrowMedia(LibraryItem media) {
+        if (media instanceof Borrowable) {
+            Borrowable b = (Borrowable) media;
+            if (b.isAvailable() && getBorrowedItemsCount() < getMaxBorrowLimit()) {
+                b.borrowItem(getName());
+                addBorrowedItem(media);
+                System.out.println(getName() + " (Student) borrowed: " + media.getTitle());
+            } else {
+                System.out.println("Sorry, " + media.getTitle() + " is not available");
+            }
         }
     }
 
-    public void returnMedia(Media media) {
-        media.returnItem();
-    }
-
-    public int getMaxBorrowLimit() {
-        return 3;
+    public void returnMedia(LibraryItem media) {
+        if (media instanceof Borrowable) {
+            Borrowable b = (Borrowable) media;
+            b.returnItem();
+            removeBorrowedItem(media);
+        }
     }
 }
